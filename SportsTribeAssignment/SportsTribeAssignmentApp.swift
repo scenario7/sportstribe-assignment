32b225b56e84241cbaa91180b62977d079014fdc
scenario7 <kevvin.thomas@gmail.com>
2023-06-29 15:21:47 +0530

    Initial Commit

---
+++ SportsTribeAssignment/SportsTribeAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportsTribeAssignmentApp.swift
+//  SportsTribeAssignment
+//
+//  Created by Kevin Thomas on 29/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SportsTribeAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
